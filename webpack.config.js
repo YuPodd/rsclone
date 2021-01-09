5802d50732a40673e9d9d60754e5a120fd9a67a3
Yuliya <yulia.podd@gmail.com>
2021-01-09 23:42:50 +0200

    feat: add webpack to the project

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'script.js',
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'webpack Boilerplate',
+      template: path.resolve(__dirname, './src/template.html'),
+      filename: 'index.html',
+    }),
+  ]
+};
