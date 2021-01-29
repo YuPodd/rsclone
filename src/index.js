94b3e1264c27ced458845c215d2084538a18b5ef
Yuliya <yulia.podd@gmail.com>
2021-01-29 23:51:49 +0200

    feat:add footer

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import { Tooltip, Toast, Popover } from 'bootstrap';
 import './scripts/boardsComponent/boardsMenu.js';
 import { AppComponent } from './scripts/AppComponent';
 import { showTimeAndDate } from './scripts/time';
+import { addFooterElements } from './scripts/footer';
 
 const appComponent = new AppComponent();
